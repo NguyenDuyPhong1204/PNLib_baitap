7325696d6dd7f08f0d284a87693cfdddf3c34585
NguyenDuyPhong1204 <nguyenduyphong12122004@gmail.com>
2023-10-17 00:29:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pnlib/fragment_sach.java
@@ -0,0 +1,270 @@
+package com.example.pnlib;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.pnlib.DAO.LoaiSachDAO;
+import com.example.pnlib.DAO.SachDAO;
+import com.example.pnlib.adapter.LoaiSachSpinnerAdapter;
+import com.example.pnlib.adapter.SachAdapter;
+import com.example.pnlib.model.LoaiSach;
+import com.example.pnlib.model.Sach;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class fragment_sach extends Fragment {
+    ListView lvSach;
+    SachDAO sachDAO;
+    SachAdapter adapter;
+    Sach item;
+    List<Sach> list;
+    List<Sach> tempList = new ArrayList<>();
+
+    FloatingActionButton fab;
+    Dialog dialog;
+    EditText edMaSach, edTenSach, edGiaThue, edNamXB;
+    Spinner spinner;
+    Button btnSave, btnCancel;
+
+    LoaiSachSpinnerAdapter spinnerAdapter;
+    ArrayList<LoaiSach> listLoaiSach;
+    LoaiSachDAO loaiSachDAO;
+    LoaiSach loaiSach;
+
+    Toolbar toolbar;
+    int maLoaiSach, position;
+
+
+    public fragment_sach() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_sach, container, false);
+        lvSach = v.findViewById(R.id.lvSach);
+        sachDAO = new SachDAO(getActivity());
+        capNhatLv();
+        fab = v.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog(getActivity(), 0);
+            }
+        });
+        lvSach.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                item = list.get(position);
+                openDialog(getActivity(), 1);
+                return false;
+            }
+        });
+
+        setHasOptionsMenu(true);
+
+        return v;
+    }
+
+    void capNhatLv() {
+        list = (ArrayList<Sach>) sachDAO.getAllASC();
+        tempList = sachDAO.getAll();
+        adapter = new SachAdapter(getActivity(), this, list);
+        lvSach.setAdapter(adapter);
+    }
+
+    public void xoa(final String Id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Delete");
+        builder.setMessage("Bạn có muốn xóa không?");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                sachDAO.delete(Id);
+                capNhatLv();
+                dialog.cancel();
+                Toast.makeText(getContext(), "Đã xóa", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                Toast.makeText(getContext(), "Không xóa", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        AlertDialog alert = builder.create();
+        builder.show();
+    }
+
+    protected void openDialog(final Context context, final int type) {
+        dialog = new Dialog(context);
+        dialog.setContentView(R.layout.dialog_sach);
+        edMaSach = dialog.findViewById(R.id.edMaSach);
+        edTenSach = dialog.findViewById(R.id.edTenSach);
+        edGiaThue = dialog.findViewById(R.id.edGiaThue);
+        edNamXB = dialog.findViewById(R.id.edNamXB);
+        spinner = dialog.findViewById(R.id.spLoaiSach);
+        btnCancel = dialog.findViewById(R.id.btnCancelSach);
+        btnSave = dialog.findViewById(R.id.btnSaveSach);
+        toolbar = dialog.findViewById(R.id.toolbar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if(activity != null){
+            activity.setSupportActionBar(toolbar);
+        }
+
+        listLoaiSach = new ArrayList<LoaiSach>();
+        loaiSachDAO = new LoaiSachDAO(context);
+        listLoaiSach = (ArrayList<LoaiSach>) loaiSachDAO.getAll();
+
+
+
+        spinnerAdapter = new LoaiSachSpinnerAdapter(context, listLoaiSach);
+        spinner.setAdapter(spinnerAdapter);
+        // lay maLoaiSach
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                maLoaiSach = listLoaiSach.get(position).getMaLoai();
+//                Toast.makeText(context, "Chọn "+listLoaiSach.get(position).getTenLoai(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        // kiem tra tupe insert hay update
+        edMaSach.setEnabled(false);
+        if (type != 0) {
+            edMaSach.setText(String.valueOf(item.getMaSach()));
+            edTenSach.setText(item.getTenSach());
+            edGiaThue.setText(String.valueOf(item.getGiaThue()));
+            edNamXB.setText(String.valueOf(item.getNamXB()));
+            for (int i = 0; i < listLoaiSach.size(); i++)
+                if (item.getMaLoai() == (listLoaiSach.get(i).getMaLoai())) {
+                    position = i;
+                }
+            Log.i("demo", "posSach " + position);
+            spinner.setSelection(position);
+        }
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                item = new Sach();
+                item.setTenSach(edTenSach.getText().toString());
+                item.setGiaThue(parseInt(edGiaThue.getText().toString(), 0));
+                item.setMaLoai(maLoaiSach);
+                item.setNamXB(Integer.parseInt(edNamXB.getText().toString()));
+                if (validate() > 0) {
+                    if (type == 0) {
+                        if (sachDAO.insert(item) > 0) {
+                            Toast.makeText(context, "Thêm thành công", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "Thêm thất bại", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        item.setMaSach(Integer.parseInt(edMaSach.getText().toString()));
+                        if (sachDAO.update(item) > 0) {
+                            Toast.makeText(context, "Sửa thành công", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "Sứa thất bại", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    capNhatLv();
+                    dialog.dismiss();
+                }
+
+            }
+        });
+        dialog.show();
+    }
+
+    public int validate() {
+        int check = 1;
+        if (edTenSach.getText().length() == 0 || edGiaThue.getText().length() == 0 || edNamXB.getText().length() == 0) {
+            Toast.makeText(getContext(), "Bạn phải nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+            check = -1;
+        }
+        return check;
+    }
+
+    public static int parseInt(String string, int defaultValue) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_tim,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+               return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                list.clear();
+                for(Sach sach: tempList){
+                    if(sach.getTenSach().toLowerCase().contains(newText.toLowerCase())){
+                        list.add(sach);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/pnlib/model/Sach.java
@@ -0,0 +1,59 @@
+package com.example.pnlib.model;
+
+public class Sach {
+    private int maSach;
+    private String tenSach;
+    private int giaThue;
+    private int maLoai;
+    private int namXB;
+    public Sach() {
+    }
+
+    public Sach(int maSach, String tenSach, int giaThue, int maLoai, int namXB) {
+        this.maSach = maSach;
+        this.tenSach = tenSach;
+        this.giaThue = giaThue;
+        this.maLoai = maLoai;
+        this.namXB = namXB;
+    }
+
+    public int getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(int maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public int getGiaThue() {
+        return giaThue;
+    }
+
+    public void setGiaThue(int giaThue) {
+        this.giaThue = giaThue;
+    }
+
+    public int getMaLoai() {
+        return maLoai;
+    }
+
+    public void setMaLoai(int maLoai) {
+        this.maLoai = maLoai;
+    }
+
+    public int getNamXB() {
+        return namXB;
+    }
+
+    public void setNamXB(int namXB) {
+        this.namXB = namXB;
+    }
+}
